e07cce7d5af956dbc367a20e3d8f23dc9a558d30
Jake Mercer <jake.mercer@gmail.com>
2017-11-05 09:57:06 -0500

    manual reset

--- GuildCars - STILL IN PROGRESS/Scripts/CarDealershipCreateDb.sql
+++
@@ -1,9 +0,0 @@
-USE master
-GO
-
-IF EXISTS(SELECT * FROM sys.databases WHERE name ='CarDealership')
-DROP DATABASE CarDealership
-GO
-
-CREATE DATABASE CarDealership
-GO
--- HarborMasterFromEmployeeMgmtCA/HarborMasterFromEmployeeMgmtCA/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HarborMasterFromEmployeeMgmtCA.MvcApplication" Language="C#" %>
